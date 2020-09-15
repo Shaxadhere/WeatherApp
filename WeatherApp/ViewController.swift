8a7244e21e0171a9afb2c6b67967c94ff129ae52
Shehzad <shaxad.here@gmail.com>
2020-09-15 13:44:51 +0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Shehzad on 15/09/2020.
+//  Copyright © 2020 Shehzad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
